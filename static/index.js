8c4cacb548dbbebe29c4a2b30a26827985eca07a
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2023-07-08 23:10:34 +0800

    - fixed main layout - added animations on section focus - updated skills content

--- static/index.js
+++ static/index.js
@@ -1,47 +1,20 @@
 const navLinks = document.querySelectorAll(".nav-bar a");
 const header = document.querySelector(".nav-wrapper");
-// let headerScroll = false;
-// window.onscroll = function () {
-//     if(window.scrollY  > (header.offsetHeight)) {
-//         header.classList.add("nav-scrolled")
-//         navLinks.forEach(function(link) {
-//             link.style.color = "#FFFFFFCC";
-//         })
-//     }else{
-//         header.classList.remove("nav-scrolled")
-//         navLinks.forEach(function(link) {
-//             link.style.color = "#404040CC";
-//         })
-//
-//
-//     }
-//
-// }
 
 let prevScrollpos = window.scrollY;
+let prevTarget;
+
 window.onscroll = function() {
     let currentScrollPos = window.scrollY;
     if(window.scrollY  > (header.offsetHeight)) {
         header.classList.add("nav-scrolled");
-        // header.style.backgroundColor = "var(--primary)";
-        // navLinks.forEach(function(link) {
-        //     link.style.color = "#FFFFFFCC";
-        // })
-
-
     }else{
-        // header.style.backgroundColor = "white";
-        // navLinks.forEach(function(link) {
-        //     link.style.color = "#404040CC";
-        // })
         header.classList.remove("nav-scrolled");
-
-
     }
+
+
     if (prevScrollpos < currentScrollPos) {
         header.style.top = "-100%";
-
-
     } else {
         header.style.top = "0px";
     }
@@ -49,12 +22,18 @@ window.onscroll = function() {
 }
 
 
-
 navLinks.forEach(function(navElement){
     navElement.onclick = function (e) {
         e.preventDefault();
         const target = document.getElementById(navElement.dataset.element);
         target.scrollIntoView({behavior:'smooth'});
+        if (prevTarget){
+            prevTarget.classList.remove("section-focused");
+        }
+        target.classList.add("section-focused");
+
+        target.s
+        prevTarget = target;
 
     }
 })
